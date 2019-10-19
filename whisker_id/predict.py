f156f3d21a4ae50ff247fb94623c3ca27c6576f4
Agustin Mautone <mautone@tryolabs.com>
2019-10-19 09:29:40 -0300

    add side parameter to whisker and image ids

--- whisker_id/predict.py
+++ whisker_id/predict.py
@@ -1,3 +1,4 @@
+import re
 import time
 from pathlib import Path
 
@@ -57,42 +58,46 @@ def predict(
     gallery_path,
     whisker_spot_model_path,
     n,
+    side,
     algorithm="sinkhorn",
     lion_subset=None,
     force_cpu=False,
 ):
     assert algorithm == "sinkhorn" or algorithm == "cpd"
+    assert side == "left" or side == "right"
 
     query_image_set_path = Path(query_image_set_path)
     gallery_path = Path(gallery_path)
     whisker_spot_model_path = Path(whisker_spot_model_path)
 
-    (right_whiskers, right_labels) = torch.load(gallery_path / "right_data.pt")
-    (left_whiskers, left_labels) = torch.load(gallery_path / "left_data.pt")
+    if side == "right":
+        (gal_whiskers, gal_labels) = torch.load(gallery_path / "right_data.pt")
+    else:
+        (gal_whiskers, gal_labels) = torch.load(gallery_path / "left_data.pt")
 
     inc_whiskers = get_whisker_centers(query_image_set_path, whisker_spot_model_path, force_cpu)
 
     predictions = {}
     for (img_name, probe_whisker) in tqdm(inc_whiskers):
         distances = []
+        for gal_whisker, gal_label in zip(gal_whiskers, gal_labels):
+            if lion_subset is None or gal_label in lion_subset:
+                if algorithm == "cpd":
+                    cloud_dist = hausdorff_distance(gal_whisker, probe_whisker)
+                else:
+                    cloud_dist = sinkhorn_distance(gal_whisker, probe_whisker)
 
-        for gal_whiskers, gal_labels in [
-            (right_whiskers, right_labels),
-            (left_whiskers, left_labels),
-        ]:
-            for gal_whisker, gal_label in zip(gal_whiskers, gal_labels):
-                if lion_subset is None or gal_label in lion_subset:
-                    if algorithm == "cpd":
-                        cloud_dist = hausdorff_distance(gal_whisker, probe_whisker)
-                    else:
-                        cloud_dist = sinkhorn_distance(gal_whisker, probe_whisker)
+                cloud_dist = 1 / cloud_dist if cloud_dist > 0 else 0
+                distances.append((cloud_dist, gal_label))
 
-                    cloud_dist = 1 / cloud_dist if cloud_dist > 0 else 0
-                    distances.append((cloud_dist, gal_label))
+        try:
+            image_id = int(re.search(r"image_(\d*)", img_name).group(1))
+        except ValueError:
+            raise Exception("Could not parse image id from image name.")
 
         distances = sorted(distances, key=lambda x: x[0])
         distances = list(zip(*distances))
-        predictions[img_name] = get_k_neighbors(distances[1], distances[0], n)
+        predictions[image_id] = get_k_neighbors(distances[1], distances[0], n)
 
     return predictions
 
@@ -104,7 +109,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "query_image_set_path", help="Path to the folder containing the labeled images"
     )
-    parser.add_argument("n", default=3, help="How many lions to retrive per image")
+    parser.add_argument("n", help="How many lions to retrive per image")
     parser.add_argument(
         "gallery_path",
         help="Path to the gallery, contains: right_data.pt, left_data.pt and whisker_image_ids.pt",
@@ -113,6 +118,7 @@ if __name__ == "__main__":
         "whisker_spot_model_path",
         help="Path to the folder where the models and gallery will be saved",
     )
+    parser.add_argument("side", help="Which side do the whisker images belong to (right or left).")
     parser.add_argument(
         "--algorithm",
         default="sinkhorn",
@@ -138,6 +144,7 @@ if __name__ == "__main__":
         args.gallery_path,
         args.whisker_spot_model_path,
         float(args.n),
+        args.side,
         args.algorithm,
         lion_subset,
         args.cpu,
